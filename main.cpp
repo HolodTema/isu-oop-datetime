2620da60854b173f0aee58429af0b4b369759916
artem <art.holodilov@gmail.com>
2026-05-07 22:51:29 +0300

    method testDateTime() in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,74 @@
 #include "quickSort.hpp"
 #include "DateTime.hpp"
 
+void testDateTime() {
+    std::cout << "Test DateTime class\n\n";
+
+    DateTime dt1;
+    std::cout << "Default constructor: " << dt1 << "\n\n";
+
+    DateTime dt2(2024, 3, 15);
+    std::cout << "Date-only constructor (2024-03-15): " << dt2 << "\n\n";
+
+    DateTime dt3(2024, 12, 31, 23, 59, 59);
+    std::cout << "Full constructor (2024-12-31T23:59:59): " << dt3 << "\n\n";
+
+    DateTime dt4(0);
+    std::cout << "Seconds constructor (0 seconds is 1900-01-01T00:00:00): " << dt4 << "\n\n";
+
+    DateTime dt5(86400);
+    std::cout << "Seconds constructor (86400 seconds is 1900-01-02T00:00:00): " << dt5 << "\n\n";
+
+    std::cout << "Check InvalidDateException:\n";
+    try {
+        DateTime dateInvalid(2024, 2, 30);
+        std::cout << "no exception\n";
+    } catch (const InvalidDateException& e) {
+        std::cout << e.what() << "\n";
+    }
+    std::cout << "\n";
+
+    DateTime dt2024(2024, 1, 1);
+    std::cout << "isLeapYear(2024): " << (dt2024.isLeapYear() ? "true" : "false") << "\n\n";
+
+    DateTime dt2000(2000,1,1);
+    std::cout << "isLeapYear(2000): " << (dt2000.isLeapYear() ? "true" : "false") << "\n\n";
+
+    DateTime dt1900(1900,1,1);
+    std::cout << "isLeapYear(1900): " << (dt1900.isLeapYear() ? "true" : "false") << "\n\n";
+
+    DateTime dtFeb2024(2024,2,1);
+    std::cout << "Days in February 2024: " << dtFeb2024.getAmountDaysInMonth(2) << "\n\n";
+
+    DateTime dtFeb2023(2023,2,1);
+    std::cout << "Days in February 2023: " << dtFeb2023.getAmountDaysInMonth(2) << "\n\n";
+
+    DateTime dtMonday(2026,5,9);
+    std::cout << "2026-05-09 must be Saturday: " << dtMonday.getDayOfWeek() << "\n\n";
+    DateTime dtSunday(1900,1,7);
+    std::cout << "1900-01-07 must be Sunday: " << dtSunday.getDayOfWeek() << "\n\n";
+
+    std::cout << "Test operator + \n";
+    DateTime dtDecember31(2024,12,31);
+    DateTime dtJanuary01 = dtDecember31 + 1;
+    std::cout << "2024-12-31 + 1 day = " << dtJanuary01 << "\n\n";
+
+    std::cout << "Test operator - \n";
+    int diffDays = dtJanuary01 - dtDecember31;
+    std::cout << "Difference (2025-01-01 - 2024-12-31) in days: " << diffDays << "\n\n";
+
+    DateTime dateA(2024,6,1);
+    DateTime dateB(2024,6,2);
+    std::cout << "Test compare-operators:\n";
+    std::cout << "date a = " << dateA << "\n";
+    std::cout << "date b = " << dateB << "\n";
+    std::cout << "a < b: " << (dateA < dateB ? "true" : "false") << "\n";
+    std::cout << "a > b: " << (dateA > dateB ? "true" : "false") << "\n";
+    std::cout << "a == b: " << (dateA == dateB ? "true" : "false") << "\n";
+    std::cout << "a <= b: " << (dateA <= dateB ? "true" : "false") << "\n";
+    std::cout << "b >= a: " << (dateB >= dateA ? "true" : "false") << "\n";
+}
+
 void sortDatesFromFile() {
     std::ifstream ifs("./dates.txt");
 
@@ -91,8 +159,10 @@ void calculateCatholicPasha(size_t year) {
 }
 
 int main() {
+    testDateTime();
+
     // sortDatesFromFile();
 
-    calculateCatholicPasha(2026);
+    // calculateCatholicPasha(2026);
     return 0;
 }
